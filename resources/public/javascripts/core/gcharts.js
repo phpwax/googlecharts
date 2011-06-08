b20e5d24645a996c3505b3f62c8eab9525a0a7b7
charlesmarshall <cm56marshall@googlemail.com>
2011-06-08 19:38:02 +0100

    the main js file. jquery events to init, setup and then draw the graphs using google

---
+++ resources/public/javascripts/core/gcharts.js
@@ -0,0 +1,70 @@
+/*** jquery triggers **/
+
+/**
+ * init - simply creates a google data object
+ */
+jQuery(document).bind("_g.chart", function(e, setup){
+  var graph = jQuery(e.target),
+      config = graph.data("_g.config"),
+      chart = setup|| new google.visualization[config.type](document.getElementById(graph.attr("id")));
+      ;
+  graph.data("_g.chart", chart);
+});
+/**
+ * runs over the data & cols and calls the draw command
+ */
+jQuery(document).bind("_g.draw", function(e, runall){
+  var graph = jQuery(e.target), table, options, chart;
+  //call all of the data setup functions if asked to
+  if(runall) graph.trigger("_g.options").trigger("_g.table").trigger("_g.cols").trigger("_g.data");
+  
+  table = graph.data("_g.table");
+  options = graph.data("_g.options");
+  chart = graph.data("_g.chart");
+  
+  chart.draw(table, options);
+});
+/**
+ * set the data table object
+ */
+jQuery(document).bind("_g.options", function(e, setup){
+  var graph = jQuery(e.target),
+      config = graph.data("_g.config"),
+      options = setup||config.options
+      ;
+  graph.data("_g.options", options);
+});
+/**
+ * set the data table object
+ */
+jQuery(document).bind("_g.table", function(e, setup){
+  var graph = jQuery(e.target),
+      table = setup||new google.visualization.DataTable();
+      ;
+  graph.data("_g.table", table);
+});
+/**
+ * set the column names
+ */
+jQuery(document).bind("_g.cols", function(e, setup){
+  var graph = jQuery(e.target),
+      config = graph.data("_g.config"),
+      cols = setup||config.columns,
+      table = graph.data("_g.table")
+      ;
+  for(var a in cols) table.addColumn(cols[a].type, cols[a].name);  
+});
+/**
+ * set the datatable on the object
+ */
+jQuery(document).bind("_g.data", function(e, setup){
+  var graph = jQuery(e.target),
+      config = graph.data("_g.config"),
+      data = setup||config.data,
+      table = graph.data("_g.table");
+      
+  table.addRows(data);    
+  graph.data("_g.table", table);
+
+});
+
